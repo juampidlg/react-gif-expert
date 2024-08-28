feacb94e132c87702632083644ea900d71180b9e
juampidlg <juampidlg@gmail.com>
2024-08-27 23:34:08 -0300

    primer commit

---
+++ src/components/Games.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react"
+
+
+export const Games = () => {
+
+    /*const [match, setMatch] = useState(null)
+
+    useEffect(()=>{
+
+    },[])*/
+
+
+  return (
+    <>
+    <div id="wg-api-football-standings"
+    data-host="v3.football.api-sports.io"
+    data-key="Your-Api-Key-Here"
+    data-league=""
+    data-team=""
+    data-season=""
+    data-theme=""
+    data-show-errors="false"
+    data-show-logos="true"
+    className="wg_loader">
+</div>
+
+    </>
+    
+  )
+}
+
